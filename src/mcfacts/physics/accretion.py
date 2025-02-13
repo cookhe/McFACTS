932606a19757d44ce7782243fce8249a4c480362
Kaila Nathaniel <37944273+spacekaila@users.noreply.github.com>
2025-02-13 12:38:29 -0500

    added docstring for mass loss function

--- src/mcfacts/physics/accretion.py
+++ src/mcfacts/physics/accretion.py
@@ -14,8 +14,33 @@ def star_wind_mass_loss(disk_star_pro_masses,
                         disk_star_pro_orbs_a,
                         disk_opacity_func,
                         timestep_duration_yr):
+    """Removes mass according to the Cantiello+ 2021 prescription
 
-    # NOTE: need to add units, check units for opacity
+    Takes initial star masses at the start of the timestep and removes mass according
+    to Eqn. 16 in Cantiello+ 2021
+
+    Parameters
+    ----------
+    disk_star_pro_masses : numpy.ndarray
+        Initial masses [M_sun] of stars in prograde orbits around the SMBH with :obj:`float` type.
+    disk_star_pro_log_radius : numpy.ndarray
+        Radius (log R/R_sun) of stars in prograde orbits around the SMBH with :obj:`float` type.
+    disk_star_pro_log_lum : numpy.ndarray
+        Luminosity (log L/L_sun) of stars in prograde orbits around the SMBH with :obj:`float` type.
+    disk_star_pro_orbs_a : numpy.narray
+        Semi-major axes [R_{g,SMBH}] of stars in prograde orbits around the SMBH with :obj:`float` type.
+    disk_opacity_func : function
+        Disk opacity function
+    timestep_duration_yr : float
+        Length of timestep [yr]
+
+    Returns
+    -------
+    star_new_masses : numpy.ndarray
+        New masses [M_sun] after removing mass for one timestep at specified mass loss rate with :obj:`float` type.
+    """
+
+    # Get opacity for orb_a values and add SI units
     disk_opacity = disk_opacity_func(disk_star_pro_orbs_a) * (astropy_units.meter ** 2) / astropy_units.kg
 
     # First convert quantities to SI units
@@ -25,12 +50,15 @@ def star_wind_mass_loss(disk_star_pro_masses,
     timestep_duration_yr_si = timestep_duration_yr * astropy_units.year
 
     # Calculate Eddington luminosity
-    L_Edd = 4. * np.pi * astropy_const.G * astropy_const.c * star_mass / disk_opacity
+    L_Edd = (4. * np.pi * astropy_const.G * astropy_const.c * star_mass / disk_opacity).to("Lsun")
 
     # Calculate escape speed
-    v_esc = (2. * astropy_const.G * star_mass / star_radius) ** 0.5
+    v_esc = ((2. * astropy_const.G * star_mass / star_radius) ** 0.5).to("km/s")
 
-    mdot_Edd = - (star_lum / (v_esc ** 2)) * (1 + np.tanh((star_lum - L_Edd) / (0.1 * L_Edd)))
+    tanh_argument = (star_lum - L_Edd) / (0.1 * L_Edd)
+    assert astropy_units.dimensionless_unscaled == tanh_argument.unit, "Units do not cancel out, error in luminosity calculations"
+
+    mdot_Edd = (- (star_lum / (v_esc ** 2)) * (1 + np.tanh(tanh_argument.value))).to("Msun/yr")
 
     star_new_masses = ((star_mass + (mdot_Edd * timestep_duration_yr_si)).to("Msun")).value
 
