4f81c5adecbde98474544cc9f9895ca3e17b9a54
Harrison Cook <hecook@nmsu.edu>
2025-03-13 14:51:31 -0600

    use power law for BH location draws

--- scripts/mcfacts_sim.py
+++ scripts/mcfacts_sim.py
@@ -288,8 +288,10 @@ def main():
 
         # generate initial BH parameter arrays
         print("Generate initial BH parameter arrays")
-        bh_orb_a_initial = setupdiskblackholes.setup_disk_blackholes_location(
-                disk_bh_num, opts.disk_radius_outer, opts.disk_inner_stable_circ_orb)
+        bh_orb_a_initial = setupdiskblackholes.setup_disk_blackholes_location_NSC_powerlaw(
+                disk_bh_num, opts.disk_radius_outer, opts.disk_inner_stable_circ_orb,
+                opts.smbh_mass, opts.nsc_radius_crit, opts.nsc_density_index_inner,
+                opts.nsc_density_index_outer, volume_scaling=True)
         bh_mass_initial = setupdiskblackholes.setup_disk_blackholes_masses(
                 disk_bh_num,
                 opts.nsc_imf_bh_mode, opts.nsc_imf_bh_mass_max, opts.nsc_imf_bh_powerlaw_index, opts.mass_pile_up)
@@ -1662,8 +1664,10 @@ def main():
             # Assuming captured objects are not in the inner disk? (KN)
             capture = time_passed % opts.capture_time_yr
             if capture == 0:
-                bh_orb_a_captured = setupdiskblackholes.setup_disk_blackholes_location(
-                    1, opts.disk_radius_capture_outer, opts.disk_inner_stable_circ_orb)
+                bh_orb_a_captured = setupdiskblackholes.setup_disk_blackholes_location_NSC_powerlaw(
+                    1, opts.disk_radius_capture_outer, opts.disk_inner_stable_circ_orb,
+                    opts.smbh_mass, opts.nsc_radius_crit, opts.nsc_density_index_inner,
+                    opts.nsc_density_index_outer, volume_scaling=True)
                 bh_mass_captured = setupdiskblackholes.setup_disk_blackholes_masses(
                     1, opts.nsc_imf_bh_mode, opts.nsc_imf_bh_mass_max, opts.nsc_imf_bh_powerlaw_index, opts.mass_pile_up)
                 bh_spin_captured = setupdiskblackholes.setup_disk_blackholes_spins(
