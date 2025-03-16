09d9ca4d3ff6a7028b9150c6fb6adabb41508a84
Kaila Nathaniel <37944273+spacekaila@users.noreply.github.com>
2025-03-15 22:53:16 -0400

    fixing script bugs

--- scripts/star_bh_movie_frames.py
+++ scripts/star_bh_movie_frames.py
@@ -25,14 +25,19 @@ def arg():
     return opts
 
 
-
 immortal_star_cutoff = 298
 
-def plotting(stars_orba, stars_mass, mask_immortal, bh_orba, bh_mass, bbh_orba, bbh_mass, timestep,
-             mask_label, nomask_label, bh_label, bbh_label, save_name):
+
+def plotting(stars_orba, stars_mass, mask_immortal, starsin_orba, starsin_mass, starsretro_orba, starsretro_mass,
+             bh_orba, bh_mass, bhin_orba, bhin_mass, bhretro_orba, bhretro_mass, bbh_orba, bbh_mass,
+             timestep, mask_label, nomask_label, bh_label, bbh_label, save_name):
     plt.scatter(stars_orba[~mask_immortal], stars_mass[~mask_immortal], marker="o", edgecolor='#DA627D', facecolor='None', zorder=10)
     plt.scatter(stars_orba[mask_immortal], stars_mass[mask_immortal], marker="o", edgecolor='#450920', facecolor='None', zorder=10)
+    plt.scatter(starsin_orba, starsin_mass, marker="o", edgecolor='#DA627D', facecolor='None', zorder=10)
+    plt.scatter(starsretro_orba, starsretro_mass, marker="o", edgecolor='#DA627D', facecolor='None', zorder=10)
     plt.scatter(bh_orba, bh_mass, marker="o", edgecolor="darkgoldenrod", facecolor="None", zorder=10)
+    plt.scatter(bhin_orba, bhin_mass, marker="o", edgecolor="darkgoldenrod", facecolor="None", zorder=10)
+    plt.scatter(bhretro_orba, bhretro_mass, marker="o", edgecolor="darkgoldenrod", facecolor="None", zorder=10)
     plt.scatter(bbh_orba, bbh_mass, marker="o", edgecolors="#005f73", facecolor="None", zorder=10)
     plt.scatter(0, -10, label=nomask_label, color="#DA627D")
     plt.scatter(0, -10, label=mask_label, color="#450920")
@@ -45,7 +50,7 @@ def plotting(stars_orba, stars_mass, mask_immortal, bh_orba, bh_mass, bbh_orba, 
     plt.ylabel(r"Mass [$M_\odot$]")
     plt.xlabel(r"Semi-major axis [R$_{\mathrm {g, SMBH}}$]")
     plt.ylim(-5, 500)
-    plt.xlim(70, 55000)
+    plt.xlim(20, 55000)
 
     plt.xscale("log")
     plt.savefig(save_name + "_log.png", dpi=300)
@@ -55,33 +60,71 @@ def plotting(stars_orba, stars_mass, mask_immortal, bh_orba, bh_mass, bbh_orba, 
 
 def generate_plots(fpath, num_timesteps):
     bh_single_pro = []
+    bh_inner = []
+    bh_retro = []
     bh_binary = []
     star_pro = []
+    star_inner = []
+    star_retro = []
     for i in range(0, num_timesteps):
 
         bh_single_pro.append(fpath + f"/output_bh_single_pro_{i}.dat")
+        bh_inner.append(fpath + f"/output_bh_single_inner_disk_{i}.dat")
+        bh_retro.append(fpath + f"/output_bh_single_retro_{i}.dat")
         bh_binary.append(fpath + f"/output_bh_binary_{i}.dat")
 
         star_pro.append(fpath + f"/output_stars_single_pro_{i}.dat")
+        star_inner.append(fpath + f"/output_stars_single_inner_disk_{i}.dat")
+        star_retro.append(fpath + f"/output_stars_single_retro_{i}.dat")
 
-    for star_stuff, bh_stuff, bbh_stuff in zip(enumerate(star_pro), enumerate(bh_single_pro), enumerate(bh_binary)):
+    for star_stuff, star_in_stuff, star_retro_stuff, bh_stuff, bh_in_stuff, bh_retro_stuff, bbh_stuff in zip(enumerate(star_pro), enumerate(star_inner), enumerate(star_retro), enumerate(bh_single_pro), enumerate(bh_inner), enumerate(bh_retro), enumerate(bh_binary)):
         t_star, fname_star = star_stuff
         i_bh, fname_bh = bh_stuff
         i_bbh, fname_bbh = bbh_stuff
+        i_s, fname_star_inner = star_in_stuff
+        i_s, fname_star_retro = star_retro_stuff
+        i_b, fname_bh_inner = bh_in_stuff
+        i_b, fname_bh_retro = bh_retro_stuff
         t_star = f"{t_star:02d}"
-        star = np.loadtxt(fname_star, skiprows=1)
-        bh = np.loadtxt(fname_bh, skiprows=1)
-        bbh = np.loadtxt(fname_bbh, skiprows=1)
+        star = np.loadtxt(fname_star)
+        star_in = np.loadtxt(fname_star_inner)
+        star_retro = np.loadtxt(fname_star_retro)
+        bh = np.loadtxt(fname_bh)
+        bh_in = np.loadtxt(fname_bh_inner)
+        bh_retro = np.loadtxt(fname_bh_retro)
+        bbh = np.loadtxt(fname_bbh)
         mask_immortal = star[:, 2] == immortal_star_cutoff
-        if (bbh.size == 0):
-            plotting(star[:, 1], star[:, 2], mask_immortal, bh[:, 1], bh[:, 2], None, None, t_star,
-                     r"Immortal star ($298$ $M_\odot$)", "Single prograde star", "Single prograde BH", "BBH", fpath + f"/orba_mass_movie_timestep_{t_star}")
-        elif (bbh.size == 22):
-            plotting(star[:, 1], star[:, 2], mask_immortal, bh[:, 1], bh[:, 2], bbh[9], bbh[2] + bbh[3], t_star,
-                     r"Immortal star ($298$ $M_\odot$)", "Single prograde star", "Single prograde BH", "BBH", fpath + f"/orba_mass_movie_timestep_{t_star}")
+        if bbh.size == 0:
+            bbh_orba = None
+            bbh_mass = None
+        elif bbh.size == 22:
+            bbh_orba = bbh[9]
+            bbh_mass = bbh[2]
         else:
-            plotting(star[:, 1], star[:, 2], mask_immortal, bh[:, 1], bh[:, 2], bbh[:, 9], bbh[:, 2] + bbh[:, 3], t_star,
-                     r"Immortal star ($298$ $M_\odot$)", "Single prograde star", "Single prograde BH", "BBH", fpath + f"/orba_mass_movie_timestep_{t_star}")
+            bbh_orba = bbh[:, 9]
+            bbh_mass = bbh[:, 2] + bbh[:, 3]
+        if star_in.size == 0:
+            star_in_orba = None
+            star_in_mass = None
+        elif star_in.size == 16:
+            star_in_orba = star_in[1]
+            star_in_mass = star_in[2]
+        else:
+            star_in_orba = star_in[:, 1]
+            star_in_mass = star_in[:, 2]
+        if bh_in.size == 0:
+            bh_in_orba = None
+            bh_in_mass = None
+        elif bh_in.size == 14:
+            bh_in_orba = bh_in[1]
+            bh_in_mass = bh_in[2]
+        else:
+            bh_in_orba = bh_in[:, 1]
+            bh_in_mass = bh_in[:, 2]
+        plotting(star[:, 1], star[:, 2], mask_immortal, star_in_orba, star_in_mass, star_retro[:, 1], star_retro[:, 2],
+                 bh[:, 1], bh[:, 2], bh_in_orba, bh_in_mass, bh_retro[:, 1], bh_retro[:, 2], bbh_orba, bbh_mass, t_star,
+                 r"Immortal star ($298$ $M_\odot$)", "Single prograde star", "Single prograde BH", "BBH",
+                 fpath + f"/orba_mass_movie_timestep_{t_star}")
 
 
 def main():
