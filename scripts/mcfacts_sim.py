ba5240c714d987ad0ee6033a8d4115825d8cf878
Harrison Cook <hecook@nmsu.edu>
2025-03-26 12:17:41 -0600

    update parser filename defaults
    1. for --fname-output-mergers:
        a. update what's written here to match contents of line at write
           time
        b. update help message
    2. for --fname-log:
        a. change default to mcfacts.log
        b. update help message

--- scripts/mcfacts_sim.py
+++ scripts/mcfacts_sim.py
@@ -69,8 +69,8 @@ def arg():
     parser.add_argument("--fname-output", default="output.dat",
                         help="output file (if any)", type=str)
     parser.add_argument("--fname-snapshots-bh",
-                        default="output_bh_[single|binary]_$(index).dat",
-                        help="output of BH index file ")
+                        default="output_bh_[single|binary]_pro_$(timestep_current_num).dat",
+                        help="output of BH snapshot file ")
     parser.add_argument("--save-snapshots", action='store_true')
     parser.add_argument("--verbose", action='store_true')
     parser.add_argument("-w", "--work-directory",
@@ -80,8 +80,8 @@ def arg():
                         )
     parser.add_argument("--seed", type=int, default=None,
                         help="Set the random seed. Randomly sets one if not passed. Default: None")
-    parser.add_argument("--fname-log", default="mcfacts_sim.log", type=str,
-                        help="Specify a file to save the arguments for mcfacts")
+    parser.add_argument("--fname-log", default="mcfacts.log", type=str,
+                        help="Specify a file in which to save the arguments and some runtime information. Default: mcfacts.log")
 
     # Add inifile arguments
     # Read default inifile
