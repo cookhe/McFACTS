update evolve_bin to correct units

src/mcfacts/physics/evolve_bin.py
@@ -15,7 +15,17 @@ from astropy import constants as const
 
 def surrogate(mass_1, mass_2, spin_1_mag, spin_2_mag, spin_angle_1, spin_angle_2, phi_12, bin_sep, bin_inc, bin_phase, bin_orb_a, mass_SMBH, spin_SMBH, surrogate):
 
-    print(mass_1, mass_2, spin_1_mag, spin_2_mag, spin_angle_1, spin_angle_2, phi_12, bin_sep, bin_inc, bin_phase, bin_orb_a, mass_SMBH, spin_SMBH, surrogate)
+    #print(mass_1, mass_2, spin_1_mag, spin_2_mag, spin_angle_1, spin_angle_2, phi_12, bin_sep, bin_inc, bin_phase, bin_orb_a, mass_SMBH, spin_SMBH, surrogate)
+    
+    mass_1 = mass_1[0]
+    mass_2 = mass_2[0]
+    spin_1_mag = spin_1_mag[0]
+    spin_2_mag = spin_2_mag[0]
+    spin_angle_1 = spin_angle_1[0]
+    spin_angle_2 = spin_angle_2[0]
+    phi_12 = phi_12[0]
+    
+    #print(mass_1, mass_2, spin_1_mag, spin_2_mag, spin_angle_1, spin_angle_2, phi_12, bin_sep, bin_inc, bin_phase, bin_orb_a, mass_SMBH, spin_SMBH, surrogate)
     
     start = time.time()
     M_f, spin_f, v_f = evolve_binary.evolve_binary(
@@ -41,7 +51,19 @@ def surrogate(mass_1, mass_2, spin_1_mag, spin_2_mag, spin_angle_1, spin_angle_2
     run_time = end - start
     print("Merger took ", run_time, " seconds")
     
-    spin_f_mag = np.linalg.norm(spin_f) * 100
-    v_f_mag = np.linalg.norm(v_f) * 100
-  
+    spin_f_mag = np.linalg.norm(spin_f)
+    v_f_mag = np.linalg.norm(v_f) * const.c.value / 1000
+    
+    #print(M_f, spin_f_mag, v_f_mag)
+    
+    M_f = np.array([M_f])
+    spin_f_mag = np.array([spin_f_mag])
+    v_f_mag = np.array([v_f_mag])
+    
+    #print(M_f, spin_f_mag, v_f_mag)
+    
+    print("M_f = ", M_f)
+    print("spin_f = ", spin_f_mag)
+    print("v_f = ", v_f_mag)
+    
     return M_f, spin_f_mag, v_f_mag