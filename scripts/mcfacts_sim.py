8aa4a5104fb5e6a69518fd8fd84ad60eaf92ec0f
Kaila Nathaniel <37944273+spacekaila@users.noreply.github.com>
2025-07-08 11:49:11 -0400

    fixed bug in disk mass output file writing

--- scripts/mcfacts_sim.py
+++ scripts/mcfacts_sim.py
@@ -3102,7 +3102,7 @@ def main():
             stars_unbound.to_txt(os.path.join(opts.work_directory, stars_unbound_save_name), stars_cols)
             temp_mass_cycled = np.column_stack((np.full(len(disk_arr_timestep), galaxy), disk_arr_timestep, disk_arr_mass_gained, disk_arr_mass_lost))
             with open(os.path.join(opts.work_directory, disk_mass_cycled_save_name), "a") as file:
-                np.savetxt(os.path.join(opts.work_directory, disk_mass_cycled_save_name), temp_mass_cycled)
+                np.savetxt(os.path.join(file, temp_mass_cycled))
 
     toc_perf = time.perf_counter()
     print("Perf time: %0.2f"%(toc_perf - tic_perf))
